5a098cb936e5e878419fb6c8ff9b702265d38b57
Philippe Ouimet <philippe.ouimet@yaksa.ca>
2018-06-06 15:50:01 +0000

    Initial commit

---
+++ functions/funfacts.js
@@ -0,0 +1,408 @@
+module.exports = {
+    FUN_FACTS_EN: [{
+    "type": "success",
+    "value": [
+        
+        {
+        "id": 550,
+        "joke": "%s can speak Braille.",
+        "categories": []
+    }, {
+        "id": 473,
+        "joke": "%s can overflow your stack just by looking at it.",
+        "categories": ["nerdy"]
+    }, {
+        "id": 425,
+        "joke": "%s is the only known mammal in history to have an opposable thumb. On his penis.",
+        "categories": []
+    }, {
+        "id": 536,
+        "joke": "Don't worry about tests, %s's test cases cover your code too.",
+        "categories": ["nerdy"]
+    }, {
+        "id": 154,
+        "joke": "Movie trivia: The movie &quot;Invasion U.S.A.&quot; is, in fact, a documentary.",
+        "categories": []
+    }, {
+        "id": 519,
+        "joke": "%s once won a game of connect four in 3 moves.",
+        "categories": []
+    }, {
+        "id": 308,
+        "joke": "When you say &quot;no one's perfect&quot;, %s takes this as a personal insult.",
+        "categories": []
+    }, {
+        "id": 25,
+        "joke": "In the Bible, Jesus turned water into wine. But then %s turned that wine into beer.",
+        "categories": []
+    }, {
+        "id": 195,
+        "joke": "%s wears a live rattlesnake as a condom.",
+        "categories": []
+    }, {
+        "id": 488,
+        "joke": "%s doesn't pair program.",
+        "categories": ["nerdy"]
+    }, {
+        "id": 421,
+        "joke": "%s is not Politically Correct. He is just Correct. Always.",
+        "categories": []
+    }, {
+        "id": 518,
+        "joke": "%s doesn't cheat death. He wins fair and square.",
+        "categories": []
+    }, {
+        "id": 336,
+        "joke": "%s uses tabasco sauce instead of visine.",
+        "categories": []
+    }, {
+        "id": 592,
+        "joke": "%s can make onions cry.",
+        "categories": []
+    }, {
+        "id": 312,
+        "joke": "Jesus can walk on water, but %s can walk on Jesus.",
+        "categories": []
+    }, {
+        "id": 236,
+        "joke": "Sticks and stones may break your bones, but a %s glare will liquefy your kidneys.",
+        "categories": []
+    }, {
+        "id": 468,
+        "joke": "%s's beard can type 140 wpm.",
+        "categories": ["nerdy"]
+    }, {
+        "id": 5,
+        "joke": "%s lost his virginity before his dad did.",
+        "categories": ["explicit"]
+    }, {
+        "id": 565,
+        "joke": "%s can make a class that is both abstract and final.",
+        "categories": ["nerdy"]
+    }, {
+        "id": 39,
+        "joke": "%s will attain statehood in 2009. His state flower will be the Magnolia.",
+        "categories": []
+    }, {
+        "id": 224,
+        "joke": "When %s plays Oregon Trail, his family does not die from cholera or dysentery, but rather, roundhouse kicks to the face. He also requires no wagon, since he carries the oxen, axels, and buffalo meat on his back. He always makes it to Oregon before you.",
+        "categories": []
+    }, {
+        "id": 263,
+        "joke": "The original title for Star Wars was &quot;Skywalker: Texas Ranger&quot;. Starring %s.",
+        "categories": []
+    }, {
+        "id": 55,
+        "joke": "Most people have 23 pairs of chromosomes. %s has 72... and they're all poisonous.",
+        "categories": []
+    }, {
+        "id": 191,
+        "joke": "The easiest way to determine %s' age is to cut him in half and count the rings.",
+        "categories": []
+    }, {
+        "id": 186,
+        "joke": "Fear is not the only emotion %s can smell. He can also detect hope, as in &quot;I hope I don't get a roundhouse kick from %s.&quot;",
+        "categories": []
+    }, {
+        "id": 385,
+        "joke": "%s once rode a bull, and nine months later it had a calf.",
+        "categories": []
+    }, {
+        "id": 384,
+        "joke": "%s was the orginal sculptor of Mount Rushmore. He completed the entire project using only a bottle opener and a drywall trowel.",
+        "categories": []
+    }, {
+        "id": 168,
+        "joke": "%s is the only person in the world that can actually email a roundhouse kick.",
+        "categories": []
+    }, {
+        "id": 15,
+        "joke": "When %s goes to donate blood, he declines the syringe, and instead requests a hand gun and a bucket.",
+        "categories": []
+    }, {
+        "id": 531,
+        "joke": "Jesus can walk on water, but %s can swim through land.",
+        "categories": []
+    }, {
+        "id": 313,
+        "joke": "All roads lead to %s. And by the transitive property, a roundhouse kick to the face.",
+        "categories": []
+    }, {
+        "id": 34,
+        "joke": "The opening scene of the movie &quot;Saving Private Ryan&quot; is loosely based on games of dodgeball %s played in second grade.",
+        "categories": []
+    }, {
+        "id": 585,
+        "joke": "%s does not need a watch, he decides what time it is.",
+        "categories": []
+    }, {
+        "id": 309,
+        "joke": "%s can win a game of Trivial Pursuit with one roll of the dice, and without answering a single question... just a nod of the head, and a stroke of the beard.",
+        "categories": []
+    }, {
+        "id": 253,
+        "joke": "Coroners refer to dead people as &quot;ABC's&quot;. Already Been Chucked.",
+        "categories": []
+    }, {
+        "id": 403,
+        "joke": "%s crossed the road. No one has ever dared question his motives.",
+        "categories": []
+    }, {
+        "id": 397,
+        "joke": "When %s makes a burrito, its main ingredient is real toes.",
+        "categories": []
+    }, {
+        "id": 513,
+        "joke": "%s does not code in cycles, he codes in strikes.",
+        "categories": ["nerdy"]
+    }, {
+        "id": 503,
+        "joke": "%s protocol design method has no status, requests or responses, only commands.",
+        "categories": ["nerdy"]
+    }, {
+        "id": 243,
+        "joke": "Contrary to popular belief, the Titanic didn't hit an iceberg. The ship was off course and ran into %s while he was doing the backstroke across the Atlantic.",
+        "categories": []
+    }, {
+        "id": 404,
+        "joke": "When %s was born, he immediately had sex with the first nurse he saw. He was her first. She was his third. That afternoon.",
+        "categories": []
+    }, {
+        "id": 281,
+        "joke": "4 out of 5 doctors fail to recommend %s as a solution to most problems. Also, 80% of doctors die unexplained, needlessly brutal deaths.",
+        "categories": []
+    }, {
+        "id": 42,
+        "joke": "%s doesn't churn butter. He roundhouse kicks the cows and the butter comes straight out.",
+        "categories": []
+    }, {
+        "id": 51,
+        "joke": "The quickest way to a man's heart is with %s' fist.",
+        "categories": []
+    }, {
+        "id": 40,
+        "joke": "A handicapped parking sign does not signify that this spot is for handicapped people. It is actually in fact a warning, that the spot belongs to %s and that you will be handicapped if you park there.",
+        "categories": []
+    }, {
+        "id": 3,
+        "joke": "%s doesn't read books. He stares them down until he gets the information he wants.",
+        "categories": []
+    }, {
+        "id": 467,
+        "joke": "%s can delete the Recycling Bin.",
+        "categories": ["nerdy"]
+    }, {
+        "id": 416,
+        "joke": "%s plays racquetball with a waffle iron and a bowling ball.",
+        "categories": []
+    }, {
+        "id": 507,
+        "joke": "%s can install iTunes without installing Quicktime.",
+        "categories": ["nerdy"]
+    }, {
+        "id": 166,
+        "joke": "%s doesn't play god. Playing is for children.",
+        "categories": []
+    }, {
+        "id": 189,
+        "joke": "Behind every successful man, there is a woman. Behind every dead man, there is %s.",
+        "categories": []
+    }, {
+        "id": 525,
+        "joke": "%s can retrieve anything from /dev/null.",
+        "categories": ["nerdy"]
+    }, {
+        "id": 392,
+        "joke": "After returning from World War 2 unscrathed, Bob Dole was congratulated by %s with a handshake. The rest is history.",
+        "categories": []
+    }, {
+        "id": 223,
+        "joke": "%s smells what the Rock is cooking... because the Rock is %s' personal chef.",
+        "categories": []
+    }, {
+        "id": 543,
+        "joke": "%s's programs can pass the Turing Test by staring at the interrogator.",
+        "categories": ["nerdy"]
+    }, {
+        "id": 430,
+        "joke": "%s does not wear a condom. Because there is no such thing as protection from %s.",
+        "categories": []
+    }, {
+        "id": 107,
+        "joke": "%s doesn't bowl strikes, he just knocks down one pin and the other nine faint.",
+        "categories": []
+    }, {
+        "id": 372,
+        "joke": "%s once rode a nine foot grizzly bear through an automatic car wash, instead of taking a shower.",
+        "categories": []
+    }, {
+        "id": 91,
+        "joke": "When taking the SAT, write &quot;%s&quot; for every answer. You will score over 8000.",
+        "categories": []
+    }, {
+        "id": 335,
+        "joke": "%s likes his coffee half and half: half coffee grounds, half wood-grain alcohol.",
+        "categories": []
+    }, {
+        "id": 26,
+        "joke": "%s is the only human being to display the Heisenberg uncertainty principle - you can never know both exactly where and how quickly he will roundhouse-kick you in the face.",
+        "categories": ["nerdy"]
+    }, {
+        "id": 136,
+        "joke": "%s does not own a stove, oven, or microwave , because revenge is a dish best served cold.",
+        "categories": []
+    }, {
+        "id": 464,
+        "joke": "&quot;It works on my machine&quot; always holds true for %s.",
+        "categories": ["nerdy"]
+    }, {
+        "id": 209,
+        "joke": "That's not %s doing push-ups -- that's %s moving the Earth away from the path of a deadly asteroid.",
+        "categories": []
+    }, {
+        "id": 477,
+        "joke": "%s can access private methods.",
+        "categories": ["nerdy"]
+    }, {
+        "id": 114,
+        "joke": "%s doesn't believe in Germany.",
+        "categories": []
+    }, {
+        "id": 96,
+        "joke": "Nobody doesn't like Sara Lee. Except %s.",
+        "categories": []
+    }, {
+        "id": 429,
+        "joke": "%s has never been accused of murder because his roundhouse kicks are recognized as &quot;acts of God.&quot;",
+        "categories": []
+    }, {
+        "id": 334,
+        "joke": "%s qualified with a top speed of 324 mph at the Daytona 500, without a car.",
+        "categories": []
+    }, {
+        "id": 548,
+        "joke": "Product Owners never argue with %s after he demonstrates the DropKick feature.",
+        "categories": ["nerdy"]
+    }, {
+        "id": 138,
+        "joke": "%s can slam a revolving door.",
+        "categories": []
+    }, {
+        "id": 70,
+        "joke": "%s' house has no doors, only walls that he walks through.",
+        "categories": []
+    }, {
+        "id": 505,
+        "joke": "%s can spawn threads that complete before they are started.",
+        "categories": ["nerdy"]
+    }, {
+        "id": 188,
+        "joke": "MacGyver can build an airplane out of gum and paper clips, but %s can roundhouse-kick his head through a wall and take it.",
+        "categories": []
+    }, {
+        "id": 526,
+        "joke": "No one has ever pair-programmed with %s and lived to tell about it.",
+        "categories": ["nerdy"]
+    }, {
+        "id": 440,
+        "joke": "Most tough men eat nails for breakfast. %s does all of his grocery shopping at Home Depot.",
+        "categories": []
+    }, {
+        "id": 127,
+        "joke": "%s invented the bolt-action rifle, liquor, sexual intercourse, and football-- in that order.",
+        "categories": []
+    }, {
+        "id": 456,
+        "joke": "All browsers support the hex definitions #chuck and #norris for the colors black and blue.",
+        "categories": ["nerdy"]
+    }, {
+        "id": 354,
+        "joke": "For undercover police work, %s pins his badge underneath his shirt, directly into his chest.",
+        "categories": []
+    }, {
+        "id": 433,
+        "joke": "In ancient China there is a legend that one day a child will be born from a dragon, grow to be a man, and vanquish evil from the land. That man is not %s, because %s killed that man.",
+        "categories": []
+    }, {
+        "id": 126,
+        "joke": "When %s does a pushup, he isn't lifting himself up, he's pushing the Earth down.",
+        "categories": []
+    }, {
+        "id": 18,
+        "joke": "According to the Encyclopedia Brittanica, the Native American &quot;Trail of Tears&quot; has been redefined as anywhere that %s walks.",
+        "categories": []
+    }, {
+        "id": 176,
+        "joke": "According to Einstein's theory of relativity, %s can actually roundhouse kick you yesterday.",
+        "categories": []
+    }, {
+        "id": 273,
+        "joke": "%s does not kick ass and take names. In fact, %s kicks ass and assigns the corpse a number. It is currently recorded to be in the billions.",
+        "categories": []
+    }, {
+        "id": 129,
+        "joke": "%s keeps his friends close and his enemies closer. Close enough to drop them with one round house kick to the face.",
+        "categories": []
+    }, {
+        "id": 62,
+        "joke": "Some people like to eat frogs' legs. %s likes to eat lizard legs. Hence, snakes.",
+        "categories": []
+    }, {
+        "id": 601,
+        "joke": "%s can remember the future.",
+        "categories": []
+    }, {
+        "id": 226,
+        "joke": "&quot;Brokeback Mountain&quot; is not just a movie. It's also what %s calls the pile of dead ninjas in his front yard.",
+        "categories": []
+    }, {
+        "id": 23,
+        "joke": "Time waits for no man. Unless that man is %s.",
+        "categories": []
+    }, {
+        "id": 441,
+        "joke": "%s did not &quot;lose&quot; his virginity, he stalked it and then destroyed it with extreme prejudice.",
+        "categories": []
+    }, {
+        "id": 292,
+        "joke": "If you were somehow able to land a punch on %s your entire arm would shatter upon impact. This is only in theory, since, come on, who in their right mind would try this?",
+        "categories": []
+    }, {
+        "id": 207,
+        "joke": "It is believed dinosaurs are extinct due to a giant meteor. That's true if you want to call %s a giant meteor.",
+        "categories": []
+    }, {
+        "id": 371,
+        "joke": "They once made a %s toilet paper, but there was a problem-- It wouldn't take shit from anybody.",
+        "categories": []
+    }, {
+        "id": 307,
+        "joke": "%s never has to wax his skis because they're always slick with blood.",
+        "categories": []
+    }, {
+        "id": 94,
+        "joke": "%s has the greatest Poker-Face of all time. He won the 1983 World Series of Poker, despite holding only a Joker, a Get out of Jail Free Monopoly card, a 2 of clubs, 7 of spades and a green #4 card from the game UNO.",
+        "categories": []
+    }, {
+        "id": 85,
+        "joke": "When Steven Seagal kills a ninja, he only takes its hide. When %s kills a ninja, he uses every part.",
+        "categories": []
+    }, {
+        "id": 293,
+        "joke": "70% of a human's weight is water. 70% of %s' weight is his dick.",
+        "categories": ["explicit"]
+    }, {
+        "id": 12,
+        "joke": "%s sheds his skin twice a year.",
+        "categories": []
+    }, {
+        "id": 541,
+        "joke": "When %s break the build, you can't fix it, because there is not a single line of code left.",
+        "categories": ["nerdy"]
+    }, {
+        "id": 265,
+        "joke": "The phrase 'balls to the wall' was originally conceived to describe %s entering any building smaller than an aircraft hangar.",
+        "categories": []
+    }]
+}
+]};
